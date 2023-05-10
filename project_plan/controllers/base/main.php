530a627a56f13d63869b2ee3d3282aa8d868cd2d
Alex <tarasenkalexander>
2023-05-10 15:04:43 +0300

    creating program scheme

---
+++ project_plan/controllers/base/main.php
@@ -0,0 +1,19 @@
+<?php 
+
+/**
+ * 
+
+ * if(get)
+ * {
+ *      getMiniTasks из model - получает данные для миниатюрок
+ *      создаёт содержимое представления через common/v_main
+ * }
+ * else if(post)
+ * {
+ *      Тупо отправляю в controllers/sheets/sheet.php
+ * 
+ * }
+ * 
+ * 
+ * 
+ */
